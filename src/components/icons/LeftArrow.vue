eb60d5e57842a63ae772b1bdc53f0ef74b501f50
Gina Lee <gina@thesistesting.com>
2020-11-01 23:39:34 -0500

    add left and right arrow svg (from vueper)

---
+++ src/components/icons/LeftArrow.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg viewBox="0 0 9 18"><path stroke-linecap="round" d="m8 1 l-7 8 7 8"></path></svg>
+</template>
---
+++ src/components/icons/RightArrow.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg viewBox="0 0 9 18"><path stroke-linecap="round" d="m1 1 l7 8 -7 8"></path></svg>
+</template>
